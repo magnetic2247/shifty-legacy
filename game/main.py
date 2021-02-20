232dc746312f7cd3d0cf902b72aaff47e50c4998
polix_minus <polisminus2247@gmail.com>
2021-02-20 11:37:46 +0400

    CAR IS NOT THE SCREEN :D

--- game/main.py
+++ game/main.py
@@ -1,10 +1,24 @@
+# Pygame Library
 import pygame
+
+# Keys
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 # Initialize pygame
 pygame.init()
 
-# Screen 
-screen = pygame.display.set_mode([500, 500])
+# Screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # Run until user quits
 running = True
@@ -16,6 +30,10 @@ while running:
 
     # Fill Screen to reset it
     screen.fill((0,0,0))
+
+    # CAR :D
+    car = pygame.image.load("../assets/blue_car.png")
+    screen.blit(car, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
     # Update Display
     pygame.display.flip()
