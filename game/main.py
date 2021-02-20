1c6db113f48c6713ca989a56c4c869c0dd92f605
polix_minus <polisminus2247@gmail.com>
2021-02-20 11:00:36 +0400

    Nothing much for now. Just the basic pygame structure

--- game/main.py
+++ game/main.py
@@ -1,1 +1,24 @@
 import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Screen 
+screen = pygame.display.set_mode([500, 500])
+
+# Run until user quits
+running = True
+while True:
+    # Event Loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT
+
+    # Fill Screen to reset it
+    screen.fill((0,0,0))
+
+    # Update Display
+    pygame.display.flip()
+
+# Quit!
+pygame.quit()
+    
