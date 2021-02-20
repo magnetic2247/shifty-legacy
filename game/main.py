a9ef3f880c7f240bce5aeee054d97851e25cbd14
polix_minus <polisminus2247@gmail.com>
2021-02-20 14:01:43 +0400

    First shite

--- game/main.py
+++ game/main.py
@@ -20,9 +20,24 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
+# Assets
+# Road
+mark = pygame.Surface((15,50))
+mark_pos = (10, 10)
+mark.fill((255, 255, 255))
+
+# Car :D
+car = pygame.transform.scale2x(pygame.image.load("../assets/blue_car.png"))
+
 # Run until user quits
+getTicksLastFrame = 0
 running = True
 while running:
+    # Delta Time
+    t = pygame.time.get_ticks()
+    deltaTime = (t - getTicksLastFrame) / 100
+    getTicksLastFrame = t
+
     # Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,9 +46,12 @@ while running:
     # Fill Screen to reset it
     screen.fill((0,0,0))
 
-    # CAR :D
-    car = pygame.image.load("../assets/blue_car.png")
-    screen.blit(car, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    # Road
+    mark_pos = (10, mark_pos[1]+20*deltaTime)
+    screen.blit(mark, mark_pos)
+    
+    # Car
+    screen.blit(car, (SCREEN_WIDTH/2 - car.get_width()/2, SCREEN_HEIGHT/2 - car.get_height()/2))
 
     # Update Display
     pygame.display.flip()
